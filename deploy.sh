564be63b63551459c81d0f22b1f0fa886eb85186
Gustavo Meza <gmezanun@emeal.nttdata.com>
2021-12-16 19:25:38 -0500

    other docs

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+cd "./bootcamp-account-service"
+git pull
+mvn package
+
+cd ../bootcamp-config-service/
+git pull
+mvn package
+
+cd ../bootcamp-credit-service/
+git pull
+mvn package
+
+cd ../bootcamp-eureka-service/
+git pull
+mvn package
+
+cd ../bootcamp-gateway-service
+git pull
+mvn package
+
+cd ../bootcamp-product-service
+git pull
+mvn package
+
+cd ../bootcamp-user-service
+git pull
+mvn package
+
+cd ../bootcamp-operation-service
+git pull
+mvn package
+
+cd ..
+
---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git clone https://github.com/gmeza-nttdata/bootcamp-account-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-config-data.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-config-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-credit-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-eureka-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-gateway-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-product-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-user-service.git
+
+git clone https://github.com/gmeza-nttdata/bootcamp-operation-service.git
